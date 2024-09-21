2461a338ce748de7c2b27f91a7f400be3bdc79fb
itsuki-forma <itsuki20231202shu@outlook.jp>
2024-09-21 16:51:32 +0900

    Add files via upload

---
+++ kadai-005/bmi.js
@@ -0,0 +1,12 @@
+// bmi.js
+let weight;
+weight = 68;
+
+let height;
+height = 1.7;
+
+let bmi
+bmi = (weight/[height*height]);
+
+console.log(weight/[height*height]);
+// console.log(68/[1.7*1.7]);
