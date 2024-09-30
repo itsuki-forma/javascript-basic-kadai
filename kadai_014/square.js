d31dbfc44bfcb3b8f2173515c1037b3e5151c4d9
itsuki-forma <itsuki20231202shu@outlook.jp>
2024-09-30 23:19:22 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,14 @@
+// square.js
+
+// 引数で渡した値を二乗した計算結果を戻り値として返す関数を記述してください。
+// 計算結果（関数の戻り値）はコンソールに出力してください。
+// 作業手順は後述の各Stepを参考にしてください。
+
+const square = (num) => {
+    return num ** 2;
+}
+
+console.log(square(3));
+
+// const num = 2**3;
+// console.log(num);
